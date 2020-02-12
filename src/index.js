7b255b5d0ddf797838750b49cc66f5ffd79107ca
yanvovk <55703574+yanvovk@users.noreply.github.com>
2020-02-12 17:41:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import readlineSync from 'readline-sync';
 export default () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name?');
-  playerName = name;
   console.log(`Hello, ${name}!`);
+  return name;
 };
