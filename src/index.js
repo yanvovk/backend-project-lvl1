e297c5d64d78adb9719f70a1a2af3028292b43e4
Make Love <makelove@kk.hk>
2020-02-12 13:49:24 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import readlineSync from 'readline-sync';
 
+export let player;
+
 export default () => {
   console.log('Welcome to the Brain Games!');
   const playerName = readlineSync.question('May I have your name?');
+  player = playerName;
   console.log(`Hello, ${playerName}!`);
 };
