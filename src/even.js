e70ad4fe1b50b23a0cfe53b57f2c4988b4f3f772
yanvovk <55703574+yanvovk@users.noreply.github.com>
2020-02-12 14:00:25 +0300

    Update even.js

--- src/even.js
+++ src/even.js
@@ -1,34 +1,29 @@
-import {default as hello, player as playerName} from '../src/index.js';
 import readlineSync from 'readline-sync';
+import { default as hello, player as playerName } from './index.js';
 
-const randomNum = () => {
-    return Math.floor(Math.random() * Math.floor(1000000000));
-};
+const randomNum = () => Math.floor(Math.random() * Math.floor(1000000000));
 
 const isEven = (num) => {
-    let answer = {yes: 'yes', no: 'no'};
-    return num % 2 > 0 ? answer.no : answer.yes;
-}
-
-
+  const answer = { yes: 'yes', no: 'no' };
+  return num % 2 > 0 ? answer.no : answer.yes;
+};
 
 export default () => {
-    hello();
-    console.log('Answer "yes" if the number is even, otherwise answer "no"')
-    for(let i = 0; i < 3; i += 1) {
-      let num = randomNum();
-      let right_answer = isEven(num);
-      console.log(`Question: ${num}`);
-      let player_answer = readlineSync.question('Your answer:');
-      if(player_answer !== right_answer) {
-          console.log(`${player_answer} is wrong answer ;(. Correct answer was "${right_answer}".`);
-          console.log(`Let's try again, ${playerName}!`);
-          break;
-      };
-      console.log('Correct!');
-      if(i === 2) {
-          console.log(`Congratulations, ${playerName}`);
-      };
+  hello();
+  console.log('Answer "yes" if the number is even, otherwise answer "no"')
+  for(let i = 0; i < 3; i += 1) {
+    const num = randomNum();
+    let rightAnswer = isEven(num);
+    console.log(`Question: ${num}`);
+    let playerAnswer = readlineSync.question('Your answer:');
+    if(playerAnswer !== rightAnswer) {
+        console.log(`${playerAnswer} is wrong answer ;(. Correct answer was "${rightAnswer}".`);
+        console.log(`Let's try again, ${playerName}!`);
+        break;
     };
-    return;
-}
+    console.log('Correct!');
+    if(i === 2) {
+        console.log(`Congratulations, ${playerName}`);
+    };
+  }
+};
