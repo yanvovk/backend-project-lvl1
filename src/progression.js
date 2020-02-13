d30cd35c6a5b684b2883eb49079e3d66aa53b2a1
Make Love <makelove@kk.hk>
2020-02-13 17:33:42 +0300

    fix

--- src/progression.js
+++ src/progression.js
@@ -9,25 +9,25 @@ const question = () => {
   const first = random(100);
   const delta = random(10);
   deltaG = delta;
-  const blank = random(10);
-  let arr = [];
+  const blankIndex = random(10);
+  const arr = [];
   for (let i = 0, j = first; i < 10; i += 1, j += delta) {
     arr.push(j);
   }
-  arr[delta] = '..';
+  arr[blankIndex] = '..';
   progression = arr;
   return arr.toString().replace(/,/g, ' ');
 };
 
 const rigthAnswer = () => {
   let answer = 0;
-  const blankIndex = progression.findIndex(e => e === '..');
+  const blankIndex = progression.findIndex((e) => e === '..');
   if (blankIndex === 0) {
     answer = progression[1] - deltaG;
-  };
+  }
   answer = progression[blankIndex - 1] + deltaG;
   return answer.toString();
-} 
+};
 
 export default () => {
   const playerName = hello();
