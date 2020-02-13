21faf74c89d2d47c23be0811d83632b6e8863178
Make Love <makelove@kk.hk>
2020-02-13 15:31:58 +0300

    fix

---
+++ src/gcd.js
@@ -0,0 +1,30 @@
+import hello from './index.js';
+import random from './random.js';
+import run from './run.js';
+
+const question = () => {
+  const a = random(100);
+  const b = random(100);
+  return `${a} ${b}`;
+};
+
+const gcdFinder = (str) => {
+  const [a, b] = str.split(' ');
+  let n = Number(a);
+  let m = Number(b);
+  while(n!== 0 && m !== 0) {
+    if(n > m) {
+      n = n % m;
+    }
+    m = m % n;
+  };
+  const gcd = n + m;
+  return gcd;
+};
+
+export default () => {
+  const playerName = hello();
+  console.log('What is the result of the expression?');
+  run(playerName, question, gcdFinder);
+};
+  
