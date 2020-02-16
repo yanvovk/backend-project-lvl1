9c2a628486a1cf740a4f154e721e0e970ceccb7f
yanvovk <55703574+yanvovk@users.noreply.github.com>
2020-02-16 21:32:52 +0300

    Update calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,25 +1,25 @@
 import run from '../index.js';
 import random from '../random.js';
 
+let a;
+let b;
+let randOperator;
+
 const question = (min = 0, max = 10) => {
-  const a = random(min, max);
-  const b = random(min, max);
+  a = random(min, max);
+  b = random(min, max);
   const operations = ['+', '*', '-'];
-  const randOperator = operations[random(0, 3)];
+  randOperator = operations[random(0, 3)];
   return `${a} ${randOperator} ${b}`;
 };
 
-const rigthAnswer = (strExp) => {
-  const operatorAndOperands = strExp.split(' ');
-  const first = Number(operatorAndOperands[0]);
-  const second = Number(operatorAndOperands[2]);
-  const oper = operatorAndOperands[1];
+const rigthAnswer = () => {
   const functions = {
     '+': (a, b) => a + b,
     '*': (a, b) => a * b,
     '-': (a, b) => a - b,
   };
-  return functions[oper](first, second);
+  return functions[randOperator](a, b);
 };
 
 export default () => {
